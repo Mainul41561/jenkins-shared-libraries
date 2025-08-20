779f3b6f607dcbe3924a7107027f1be1343af9e9
Mainul <72917788+Mainul41561@users.noreply.github.com>
2025-08-20 15:49:19 +0600

    Update codeCheckout.groovy

--- vars/codeCheckout.groovy
+++ vars/codeCheckout.groovy
@@ -1,1 +1,14 @@
+def call(String gitUrl, String gitBranch = "main") {
+    try {
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/${gitBranch}"]],
+            userRemoteConfigs: [[url: gitUrl]]
+        ])
+        
+        return sh(script: "git rev-parse HEAD", returnStdout: true).trim()
+    } catch (err) {
+        error "Git checkout failed for ${gitUrl} on branch ${gitBranch}: ${err}"
+    }
+}
 
