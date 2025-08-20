1e835a2ba381ccb760fd9bb3592db727b54a2aa5
Mainul <72917788+Mainul41561@users.noreply.github.com>
2025-08-20 15:56:02 +0600

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,1 +1,16 @@
+def call(String projectName, String imageTag = "latest", String dockerHubUser) {
+    try {
+        echo "🐳 Building Docker image: ${dockerHubUser}/${projectName}:${imageTag}"
+
+        sh """
+            docker build \
+              --pull \
+              --no-cache \
+              -t ${dockerHubUser}/${projectName}:${imageTag} .
+        """
+        return "${dockerHubUser}/${projectName}:${imageTag}"
+    } catch (err) {
+        error "Docker build failed for ${dockerHubUser}/${projectName}:${imageTag}: ${err}"
+    }
+}
 
