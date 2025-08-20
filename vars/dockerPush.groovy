a09c1c82bc1387665bbe817f3c18d440fc6485c3
Mainul <72917788+Mainul41561@users.noreply.github.com>
2025-08-20 16:33:35 +0600

    Update dockerPush.groovy
    fix the error

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,4 +1,4 @@
-def call(String Project, String imageTag = "latest", String dockerHubUser){
+def call(String Project, String ImageTag = "latest", String dockerHubUser){
   // replace credentialsId with ur own credentialsId
   withCredentials([usernamePassword(credentialsId: 'urcredentialsId', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
       sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
